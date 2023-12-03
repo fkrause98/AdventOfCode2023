8c195a81b7c55053197c7069953c2d260de00906
Francisco Krause Arnim <fran@Franciscos-MBP.fibertel.com.ar>
2023-12-03 18:49:34 -0300

    Working day3

--- lib/day2/day2.rb
+++ lib/day2/day2.rb
@@ -1,55 +1,3 @@
-class Game
-  def initialize(input_string, red_cubes, green_cubes, blue_cubes)
-    identifier, game_order = input_string.strip.split(":")
-    @game_id = identifier.split("Game").last.to_i
-    @game_order = game_order.split(";").map { |c| c.delete(" ").split(",") }
-    @red_cubes = red_cubes
-    @green_cubes = green_cubes
-    @blue_cubes = blue_cubes
-  end
-
-  def parse_single_game(game_array)
-    dict = { red: 0, blue: 0, green: 0 }
-    game_array.each { |string|
-      num = string.scan(/\d+/).first
-      dict[string.delete(num).to_sym] = num.to_i
-    }
-    return dict
-  end
-
-  def is_possible?
-    @game_order.inject(true) { |possible, a_game|
-      game_dict = self.parse_single_game(a_game)
-      game_dict[:red] <= @red_cubes &&
-        game_dict[:blue] <= @blue_cubes &&
-        game_dict[:green] <= @green_cubes &&
-        possible
-    }
-  end
-
-  def minimum_config
-    min_red = 0
-    min_blue = 0
-    min_green = 0
-    @game_order.each { |a_game|
-      game_dict = self.parse_single_game(a_game)
-      min_red = [game_dict[:red], min_red].max
-      min_blue = [game_dict[:blue], min_blue].max
-      min_green = [game_dict[:green], min_green].max
-    }
-    return min_red, min_blue, min_green
-  end
-
-  def minimum_power_set
-    min_red, min_blue, min_green = self.minimum_config
-    return min_red * min_blue * min_green
-  end
-
-  def id
-    return @game_id
-  end
-end
-
 class AOCSolverDay2
   def initialize(red_cubes, green_cubes, blue_cubes, games)
     @red_cubes = red_cubes
@@ -74,8 +22,60 @@ class AOCSolverDay2
   end
 end
 
-input = File.foreach("./lib/day2/input.txt")
+class Game
+  def initialize(input_string, red_cubes, green_cubes, blue_cubes)
+    identifier, game_order = input_string.strip.split(':')
+    @game_id = identifier.split('Game').last.to_i
+    @game_order = game_order.split(';').map { |c| c.delete(' ').split(',') }
+    @red_cubes = red_cubes
+    @green_cubes = green_cubes
+    @blue_cubes = blue_cubes
+  end
+
+  def parse_single_game(game_array)
+    dict = { red: 0, blue: 0, green: 0 }
+    game_array.each do |string|
+      num_positions_positions = string.scan(/\d+/).first
+      dict[string.delete(num_positions).to_sym] = num_positions.to_i
+    end
+    dict
+  end
+
+  def is_possible?
+    @game_order.inject(true) do |possible, a_game|
+      game_dict = parse_single_game(a_game)
+      game_dict[:red] <= @red_cubes &&
+        game_dict[:blue] <= @blue_cubes &&
+        game_dict[:green] <= @green_cubes &&
+        possible
+    end
+  end
+
+  def minimum_config
+    min_red = 0
+    min_blue = 0
+    min_green = 0
+    @game_order.each do |a_game|
+      game_dict = parse_single_game(a_game)
+      min_red = [game_dict[:red], min_red].max
+      min_blue = [game_dict[:blue], min_blue].max
+      min_green = [game_dict[:green], min_green].max
+    end
+    [min_red, min_blue, min_green]
+  end
+
+  def minimum_power_set
+    min_red, min_blue, min_green = minimum_config
+    min_red * min_blue * min_green
+  end
+
+  def id
+    @game_id
+  end
+end
+
+input = File.foreach('./lib/day2/input.txt')
 puts "Solution for part 1: #{AOCSolverDay2.new(12, 13, 14, input).part1}"
 
-input = File.foreach("./lib/day2/input.txt")
+input = File.foreach('./lib/day2/input.txt')
 puts "Solution for part 2: #{AOCSolverDay2.new(12, 13, 14, input).part2}"
---
+++ lib/day3/day3_test.rb
@@ -0,0 +1,105 @@
+require_relative 'day3'
+require 'test/unit'
+require 'set'
+class TestAOCDay3Part1 < Test::Unit::TestCase
+  def test01_parse_matrix
+    input_string = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+
+    rows = input_string.split("\n")
+    expected =
+      [
+        ['4', '6', '7', '.', '.', '1', '1', '4', '.', '.'],
+        ['.', '.', '.', '*', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '3', '5', '.', '.', '6', '3', '3', '.'],
+        ['.', '.', '.', '.', '.', '.', '#', '.', '.', '.'],
+        ['6', '1', '7', '*', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '+', '.', '5', '8', '.'],
+        ['.', '.', '5', '9', '2', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '7', '5', '5', '.'],
+        ['.', '.', '.', '$', '.', '*', '.', '.', '.', '.'],
+        ['.', '6', '6', '4', '.', '5', '9', '8', '.', '.']
+      ]
+
+    schematic = EngineSchematic.new rows
+    schematic.parse_input
+    11.times { |i| assert_equal(schematic.input_matrix[i], expected[i]) }
+  end
+
+  def test02_parse_matrix_find_positions
+    input_string = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+
+    # Split the input string into an array of rows
+    rows = input_string.split("\n")
+    expected =
+      [
+        [1, 3], [3, 6], [4, 3], [5, 5], [8, 3], [8, 5]
+      ]
+    schematic = EngineSchematic.new rows
+    schematic.find_symbols_positions
+    assert_equal(schematic.symbols_positions, expected)
+  end
+
+  def test03_parse_matrix_find_positions
+    input_string = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+    rows = input_string.split("\n")
+    expected =
+      [[0, 2], [2, 2], [2, 3], [2, 6], [2, 7], [4, 2], [6, 4], [7, 6], [9, 2], [9, 3], [9, 5], [9, 6]]
+    schematic = EngineSchematic.new rows
+    assert_equal(schematic.find_every_adjacent_number.to_set, expected.to_set)
+  end
+
+  def test04_find_extreme
+    input_string = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+    rows = input_string.split("\n")
+    expected =
+      [[9, 1], [9, 3]]
+    schematic = EngineSchematic.new rows
+    assert_equal(schematic.find_extremes([9, 3]), expected)
+  end
+
+  def test_05_find_every_digit
+    input_string = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+    rows = input_string.split("\n")
+    expected =
+      [[[2, 2], [2, 3]],
+       [[0, 0], [0, 2]],
+       [[2, 6], [2, 8]],
+       [[4, 0], [4, 2]],
+       [[6, 2], [6, 4]],
+       [[9, 1], [9, 3]],
+       [[9, 5], [9, 7]],
+       [[7, 6], [7, 8]]]
+    schematic = EngineSchematic.new rows
+    assert_equal(schematic.find_every_number_start_and_end, expected.to_set)
+  end
+
+  def test_06_should_return_467
+    input_string = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+    rows = input_string.split("\n")
+    positions = [[0, 0], [0, 2]]
+    schematic = EngineSchematic.new rows
+    assert_equal(schematic.get_number_at(positions), 467)
+  end
+
+  def test_solver_should_return_4361
+    input_string = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+    rows = input_string.split("\n")
+    solver = AoCSolverDay3.new(rows)
+    assert_equal(solver.part1, 4361)
+  end
+
+  def test_part2_should_return_467835
+    input_string = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+    rows = input_string.split("\n")
+    schematic = EngineSchematic.new rows
+    assert_equal(schematic.find_every_gear_ratio_number, 467_835)
+  end
+
+  def test_part2_actual_input
+    # input_string = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+    # rows = input_string.split("\n")
+    rows = File.foreach('input.txt')
+    schematic = EngineSchematic.new rows
+    assert_equal(schematic.find_every_gear_ratio_number, 467_835)
+  end
+end
