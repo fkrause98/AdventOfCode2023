d4ba2cdf2e9b2bc10936742ad2d348d8ed690210
Francisco Krause Arnim <fran@Franciscos-MBP.fibertel.com.ar>
2023-12-04 12:22:52 -0300

    Day 4 part 2 draft

--- lib/day4/day4.rb
+++ lib/day4/day4.rb
@@ -4,18 +4,29 @@ class AoCSolverDay4
   end
 
   def part1
-    @cards.map { |card| card.score }.sum
+    @cards
+      .map { |card| card.score }
+      .sum
+  end
+
+  def part2
+    @cards
+      .map { |card| [card.id, card] }
+      .to_h
   end
 end
 
 class Card
+  attr_reader :id
+
   def initialize(input)
     fetch_values(input)
   end
 
   def fetch_values(input)
-    actual_values, winning_values = input.split("|")
-    _, actual_values = actual_values.split(":")
+    actual_values, winning_values = input.split('|')
+    game_identifier, actual_values = actual_values.split(':')
+    @id = game_identifier.scan(/\d+/).first.to_i
     @actual_values = actual_values.scan(/\d+/).map { |string| string.to_i }
     @winning_values = winning_values.scan(/\d+/).map { |value| value.to_i }
   end
@@ -25,5 +36,9 @@ class Card
     return amount_of_winning_cards if amount_of_winning_cards <= 1
 
     2**(amount_of_winning_cards - 1)
+  end
+
+  def matching_cards
+    @actual_values.count { |card_value| @winning_values.include? card_value }
   end
 end
